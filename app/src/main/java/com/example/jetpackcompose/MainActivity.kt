14b5f035b25fe030b3e972f4207b1ed3f0c4e356
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-11 11:05:14 +0100

    Implement basic navigation example

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -13,9 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, RecipeListFragment())
-            .commit()
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_nav_host_fragment, RecipeListFragment())
+//            .commit()
 //        setContent {
 //            JetPackComposeTheme {
 //                // A surface container using the 'background' color from the theme
--- app/src/main/java/com/example/jetpackcompose/RecipeFragment.kt
+++ app/src/main/java/com/example/jetpackcompose/RecipeFragment.kt
@@ -4,26 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Divider
 import androidx.compose.material.Text
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,18 +24,15 @@ class RecipeListFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(Modifier.padding(16.dp)) {
+                Column(androidx.compose.ui.Modifier.padding(16.dp)) {
                     Text(
-                        text = "Recipe List",
+                        text = "Recipe Fragment",
                         style = TextStyle(
-                            fontSize = 24.sp
+                            fontSize = 24.sp,
+                            background = Color.Cyan,
+                            fontStyle = FontStyle.Italic
                         )
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = { findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment) }) {
-                        Text(text = "To Recipe Fragment")
-                    }
-
                 }
             }
         }
