c58191859f4b4d831ac7f2649e9916af945a4af5
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-11 15:53:41 +0100

    Implement first stapes for hilt as a dependency injection tool

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -8,39 +8,48 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose.network.RecipeService
 import com.example.jetpackcompose.ui.theme.JetPackComposeTheme
-import com.google.gson.GsonBuilder
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "AppDebug"
+
+    @Inject
+    lateinit var someRandomString: String
+
+    @Inject
+    lateinit var app: BaseApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val service = Retrofit.Builder()
-            .baseUrl("https://food2fork.ca/api/recipe/")
-            .addConverterFactory(
-                GsonConverterFactory
-                    .create(
-                        GsonBuilder()
-                            .create()
-                    )
-            )
-            .build()
-            .create(RecipeService::class.java)
 
-        CoroutineScope(IO).launch {
-            val recipe = service.get(
-                token = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48",
-                id = 583
-            )
-            Log.d("MainActivity", "onCreate: ${recipe.title}")
-        }
+        Log.d(TAG, "onCreate ${someRandomString}")
+        Log.d(TAG, "onCreate ${app}")
+
+//        val service = Retrofit.Builder()
+//            .baseUrl("https://food2fork.ca/api/recipe/")
+//            .addConverterFactory(
+//                GsonConverterFactory
+//                    .create(
+//                        GsonBuilder()
+//                            .create()
+//                    )
+//            )
+//            .build()
+//            .create(RecipeService::class.java)
+//
+//        CoroutineScope(IO).launch {
+//            val recipe = service.get(
+//                token = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48",
+//                id = 583
+//            )
+//            Log.d("MainActivity", "onCreate: ${recipe.title}")
+//        }
 
 //        val mapper = RecipeNetworkMapper()
 //
