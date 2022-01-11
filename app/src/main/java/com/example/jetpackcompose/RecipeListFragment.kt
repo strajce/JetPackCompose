df8672c5b7a40403d9f8f9b1b12439ed309fd91a
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-11 10:16:45 +0100

    Add compose in fragment

--- app/src/main/java/com/example/jetpackcompose/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackcompose/RecipeListFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 class RecipeListFragment : Fragment() {
@@ -12,7 +14,13 @@ class RecipeListFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_recipe_list, container, false)
+    ): View {
+        val view = ComposeView(requireContext())
+        view.apply {
+            setContent {
+                Text("Compose list item")
+            }
+        }
+        return view
     }
 }
