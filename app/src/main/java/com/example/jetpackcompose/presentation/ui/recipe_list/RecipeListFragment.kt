df8297774227e9e8133caa19bf86ed5dfe1bf596
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-12 10:42:12 +0100

    Implement MutableState in and show title in logs

--- app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcompose.presentation.ui.recipe_list
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,29 +17,39 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.example.jetpackcompose.R
+import com.example.jetpackcompose.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class RecipeListFragment : Fragment() {
 
-//    val viewModel: RecipeListViewModel by viewModels()
+    //    val viewModel: RecipeListViewModel by viewModels()
     val viewModel: RecipeListViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        println("RecipeListFragment: ${viewModel}")
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        println("RecipeListFragment: ${viewModel}")
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+//        viewModel.recipes.observe(viewLifecycleOwner, { recipes ->
+//        })
+
         return ComposeView(requireContext()).apply {
             setContent {
+
+                val recipes = viewModel.recipes.value
+
+                for (recipe in recipes) {
+                    Log.d(TAG, "onCreateView: ${recipe.title}")
+                }
                 Column(Modifier.padding(16.dp)) {
                     Text(
                         text = "Recipe List",
--- app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.jetpackcompose.presentation.ui.recipe_list
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.jetpackcompose.Repository.RecipeRepository
+import com.example.jetpackcompose.domain.model.Recipe
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -10,14 +17,20 @@ import javax.inject.Named
 class RecipeListViewModel
 @Inject
 constructor(
-    private val randomString: String,
     private val repository: RecipeRepository,
     private @Named("auth_token") val token: String,
 ) : ViewModel() {
 
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
+
     init {
-        println("VIEWMODEL : ${randomString}")
-        println("VIEWMODEL : ${repository}")
-        println("VIEWMODEL : ${token}")
+        viewModelScope.launch {
+            val recipe = repository.search(
+                token = token,
+                page = 1,
+                query = "chicken",
+            )
+            recipes.value = recipe
+        }
     }
 }
