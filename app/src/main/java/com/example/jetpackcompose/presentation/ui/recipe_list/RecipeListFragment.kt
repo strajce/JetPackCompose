033d3c8b2af3bfa5930d9810da541fce6cc76214
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-12 11:41:16 +0100

    Implement list view and show real data into list

--- app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -1,15 +1,11 @@
 package com.example.jetpackcompose.presentation.ui.recipe_list
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
@@ -19,6 +15,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.example.jetpackcompose.R
+import com.example.jetpackcompose.presentation.components.RecipeCard
 import com.example.jetpackcompose.util.TAG
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -47,22 +44,31 @@ class RecipeListFragment : Fragment() {
 
                 val recipes = viewModel.recipes.value
 
-                for (recipe in recipes) {
-                    Log.d(TAG, "onCreateView: ${recipe.title}")
+                LazyColumn{
+                     itemsIndexed(
+                         items = recipes
+                     ) {
+                         index, recipe ->
+                         RecipeCard(recipe = recipe, onClick = {})
+                     }
                 }
-                Column(Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Recipe List",
-                        style = TextStyle(
-                            fontSize = 24.sp
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = { findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment) }) {
-                        Text(text = "To Recipe Fragment")
-                    }
 
-                }
+//                for (recipe in recipes) {
+//                    Log.d(TAG, "onCreateView: ${recipe.title}")
+//                }
+//                Column(Modifier.padding(16.dp)) {
+//                    Text(
+//                        text = "Recipe List",
+//                        style = TextStyle(
+//                            fontSize = 24.sp
+//                        )
+//                    )
+//                    Spacer(modifier = Modifier.padding(10.dp))
+//                    Button(onClick = { findNavController().navigate(R.id.action_recipeListFragment_to_recipeFragment) }) {
+//                        Text(text = "To Recipe Fragment")
+//                    }
+//
+//                }
             }
         }
     }
--- app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -2,8 +2,6 @@ package com.example.jetpackcompose.presentation.ui.recipe_list
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetpackcompose.Repository.RecipeRepository
@@ -18,12 +16,16 @@ class RecipeListViewModel
 @Inject
 constructor(
     private val repository: RecipeRepository,
-    private @Named("auth_token") val token: String,
+    @Named("auth_token") private val token: String,
 ) : ViewModel() {
 
     val recipes: MutableState<List<Recipe>> = mutableStateOf(listOf())
 
     init {
+        newSearch()
+    }
+
+    private fun newSearch() {
         viewModelScope.launch {
             val recipe = repository.search(
                 token = token,
