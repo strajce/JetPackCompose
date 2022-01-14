8b5d8d628f756c0d06847bd2ff03d4ace0bb4e49
s.vuckovic <strahinja.vuckovic94@gmail.com>
2022-01-14 16:01:14 +0100

    Implement more animations

--- app/src/main/java/com/example/jetpackcompose/presentation/components/PulsingDemo.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/components/PulsingDemo.kt
@@ -1,6 +1,14 @@
 package com.example.jetpackcompose.presentation.components
 
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
 
+
+@Composable
+fun PulsingDemo() {
+    val color = MaterialTheme.colors.primary
+
+}
 object PulseAnimationDefinitions {
     enum class PulseState{
         INITIAL, FINAL
--- app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/example/jetpackcompose/presentation/ui/recipe/RecipeFragment.kt
@@ -5,19 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.animation.*
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -68,53 +70,98 @@ class RecipeFragment : Fragment() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     var count by remember { mutableStateOf(0) }
+                    var visible by remember { mutableStateOf(true) }
+                    val density = LocalDensity.current
                     Button(onClick = {
                         count++
+                        visible = !visible
                     }) {
                         Text(text = "Count forward")
                     }
                     Divider(modifier = Modifier.padding(24.dp))
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        AnimatedContent(
-                            modifier = Modifier.padding(16.dp),
-                            targetState = count,
-                            transitionSpec = {
-                                // Compare the incoming number with the previous number.
-                                if (targetState > initialState) {
-                                    // If the target number is larger, it slides up and fades in
-                                    // while the initial (smaller) number slides up and fades out.
-                                    slideInVertically { height -> height } + fadeIn() with
-                                            slideOutVertically { height -> -height } + fadeOut()
-                                } else {
-                                    // If the target number is smaller, it slides down and fades in
-                                    // while the initial number slides down and fades out.
-                                    slideInVertically { height -> -height } + fadeIn() with
-                                            slideOutVertically { height -> height } + fadeOut()
-                                }.using(
-                                    // Disable clipping since the faded slide-in/out should
-                                    // be displayed out of bounds.
-                                    SizeTransform(clip = false)
-                                )
-                            }
-                        ) { targetCount ->
-                            Text(
-                                text = "$targetCount",
-                                style = TextStyle(fontSize = 60.sp),
-                                fontWeight = FontWeight(60),
+                    AnimatedContent(
+                        modifier = Modifier.padding(16.dp),
+                        targetState = count,
+                        transitionSpec = {
+                            // Compare the incoming number with the previous number.
+                            if (targetState > initialState) {
+                                // If the target number is larger, it slides up and fades in
+                                // while the initial (smaller) number slides up and fades out.
+                                slideInVertically { height -> height } + fadeIn() with
+                                        slideOutVertically { height -> -height } + fadeOut()
+                            } else {
+                                // If the target number is smaller, it slides down and fades in
+                                // while the initial number slides down and fades out.
+                                slideInVertically { height -> -height } + fadeIn() with
+                                        slideOutVertically { height -> height } + fadeOut()
+                            }.using(
+                                // Disable clipping since the faded slide-in/out should
+                                // be displayed out of bounds.
+                                SizeTransform(clip = false)
                             )
                         }
+                    ) { targetCount ->
+                        Text(
+                            text = "$targetCount",
+                            style = TextStyle(fontSize = 60.sp),
+                            fontWeight = FontWeight(60),
+                        )
                     }
                     Divider(modifier = Modifier.padding(24.dp))
                     Button(onClick = {
                         count = if (count > 0) count - 1 else 0
+                        visible = !visible
                     }) {
                         Text(text = "Count backward")
                     }
                     Divider(modifier = Modifier.padding(24.dp))
                     Button(onClick = {
                         count = 0
+                        visible = true
                     }) {
                         Text("Reset counter")
+                    }
+                    Divider(modifier = Modifier.padding(24.dp))
+                    AnimatedVisibility(
+                        visible = visible,
+                        enter = slideInVertically {
+                            // Slide in from 40 dp from the top.
+                            with(density) { -40.dp.roundToPx() }
+                        } + expandVertically(
+                            // Expand from the top.
+                            expandFrom = Alignment.Top
+                        ) + fadeIn(
+                            // Fade in with the initial alpha of 0.3f.
+                            initialAlpha = 0.3f
+                        ),
+                        exit = slideOutVertically() + shrinkVertically() + fadeOut(),
+                    ) {
+                        Surface(
+                            modifier = Modifier
+                                .border(
+                                    width = 4.dp,
+                                    color = Color.Green,
+                                    CutCornerShape(16.dp)
+                                )
+                                .graphicsLayer {
+                                    shadowElevation = 8.dp.toPx()
+                                    shape = CutCornerShape(16.dp)
+                                    clip = true
+                                },
+                            color = MaterialTheme.colors.primary,
+                            elevation = 8.dp,
+                        ) {
+                            Text(
+                                text = "Fade in / Fade Out Animation",
+                                modifier = Modifier
+                                    .padding(16.dp),
+                                style = TextStyle(
+                                    fontSize = 24.sp,
+                                ),
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colors.secondary,
+                            )
+                        }
                     }
                 }
             }
